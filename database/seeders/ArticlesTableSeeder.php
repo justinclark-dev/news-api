5cbdcfffb32bafaa53e4a85cf4362d6209d0e24a
justinclark-dev <justinclark.dev@gmail.com>
2020-09-26 18:59:53 -0700

    Adds database test data.

--- database/seeders/ArticlesTableSeeder.php
+++ database/seeders/ArticlesTableSeeder.php
@@ -14,5 +14,22 @@ class ArticlesTableSeeder extends Seeder
     public function run()
     {
         //
+        $article = new \App\Models\Article([
+            'title' => 'News article #1 - Title',
+            'body' => 'News article #1 - Body'
+        ]);
+        $article->save();
+
+        $article = new \App\Models\Article([
+            'title' => 'News article #2 - Title',
+            'body' => 'News article #2 - Body'
+        ]);
+        $article->save();
+
+        $article = new \App\Models\Article([
+            'title' => 'News article #3 - Title',
+            'body' => 'News article #3 - Body'
+        ]);
+        $article->save();
     }
 }
